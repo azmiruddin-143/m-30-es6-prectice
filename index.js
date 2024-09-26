980c921bce17206a1584dec2cd07b6c2754c79e0
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-09-26 10:35:49 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+
+// node index.js///
+// const varible secont time no change man//
+
+//  const test = "azmir";
+//  test ="soniya"
+//  console.log(test);
+
+// const varible secont time no change man but change value index//
+// node index.js//
+
+//  const friend = ["Samin", "Hedayestulla", "Asif" ,20]
+//  friend[1] = "Nawus"
+//  friend.push("Soniya","Azmir")
+//  console.log(friend);
+
+
+
+// node index.js///
+// let varible secont time  change man//
+
+//   let man = 100;
+//   man = 200;
+//   console.log(man);
+
+
+// const varible secont time  change  array//
+// node index.js//
+let person = ["heelo","hi","who","by"]
+person = ["Hlw","Bangladesh"]
+// console.log(person);
+
+
+// node index.js//
+// object test const varible no change object man//
+// const bosto = {
+//     Name: "AZmir Uddin",
+//     Years: 20,
+//     Skills: "Programar",
+//     Education: "HSC",
+//     Selary : 23000
+// }
+// bosto = {
+//     kire : "loi",
+//     vdf : 20
+// }
+
+// console.log(bosto);
+
+
+
+// node index.js//
+// object test const varible  change object er value//
+const bosto = {
+    Name: "Azmir Uddin",
+    Years: 20,
+    Skills: "Programar",
+    Education: "HSC",
+    Selary : 23000
+}
+bosto.Skills = "Web Developer"
+console.log(bosto);
