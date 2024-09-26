845eac5b5cfae17897817f503e9ddd550eaaa9f9
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-09-26 19:33:40 +0600

    All Prectice Complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // node index.js///
 // const varible secont time no change man//
 
-//  const test = "azmir";
+//  const test = "azmir"; 
 //  test ="soniya"
 //  console.log(test);
 
@@ -122,13 +122,269 @@ person = ["Hlw", "Bangladesh"]
 //   console.log(jug);
 
 
+// simaple fuinction//
 
-
-
+//  let test = function(hi1,hi2){
+//     return hi1-hi2;
+//  }
+// console.log(test(50,25));
 
 
 
 //  Arrow Function Start//
 // / node index.js//
 
-// const arrowfunction = (number1=20, number2=30)=> number1 + number2;
+// let hello = (a, b) => a + b;
+// console.log(hello(1, 20));
+
+// / node index.js//
+
+// let arro = (number1,number2, number3) => number1+ number2-number3;
+// console.log(arro(5,25,10)); 
+
+//  const azmir = () => Math.random(10);
+//  console.log(azmir(10,20));
+
+// / node index.js//
+
+// const maltiple = (num1, num2, num3, num4, num5) => {
+//     const plas = num1 + num2;
+//     const maunas = num3 - num4;
+//     const plusmaynas = plas - maunas;
+//     const funalvalue  = plusmaynas * num5
+//     return funalvalue;
+// }
+
+// console.log(maltiple(100,66,500,900,10));
+
+
+
+// Arraw Finction test for array //
+// / node index.js//
+
+// let arry = [10,50,58,6,8,5,952,45,26,]
+// let soni = (arry)=> arry[2]= 100;
+// let man = soni(arry);
+// console.log(man);
+
+
+// Arraw Finction test for object //
+// / node index.js//
+
+
+// let ob = {
+//     name: "Asif",
+//     age: 23,
+//     skils: "Developer"
+// }
+
+// let hi = (ob) => ob
+// let final = hi(ob)
+// console.log(final);
+
+
+// Max ber kora;
+// / node index.js//
+
+// let big = [54, 415, 6, 326, 410, 0, 25, 56, 9, 25, 98, 74, 6, 3, 89, 7, 56,]
+
+// let kire = (big) => Math.max(...big)
+// let man = kire(big)
+// console.log(man);
+
+
+
+
+// / node index.js//
+
+// let bondhu = ["nagib", "siam", "asif", "sakil"]
+// let friend = bondhu;
+// friend.push("azmir")
+// console.log(friend);
+// console.log(bondhu);
+
+
+
+
+// / node index.js//
+
+// let bondhu = ["nagib", "siam", "asif", "sakil"]
+// let friend = bondhu;
+// friend.push("azmir")
+// console.log(friend);
+// console.log(bondhu);
+
+
+
+// / node index.js//
+// let bondhu = ["nagib", "siam", "asif", "sakil"]
+// let friend = [...bondhu];
+// friend.push("azmir")
+// console.log(friend);
+// console.log(bondhu);
+
+
+// / node index.js//
+//  let obj = {
+//     phone : "I phone",
+//     color : "black",
+//     Price : 150000
+//  }
+
+//  let alprice = obj.Price;
+//  let alphone = obj.phone;
+//  console.log(alprice);
+//  console.log(alphone);
+
+// / node index.js distracrasion//
+//  let obj = {
+//     phone : "I phone",
+//     color : "black",
+//     Price : 150000
+//  }
+
+//  let {color,phone:mobile} = obj
+//  console.log(color);
+//  console.log(mobile);
+
+// // / node index.js//
+// arry distraction
+// let az = [65, 562, 565]
+// let [f, s, t,] = az;
+// let man = f + s + t;
+// console.log(man);
+
+
+
+// // / node index.js//
+// arry distraction
+
+// let [x,y] = [10,20]
+// console.log(y);
+
+
+// herd prectice//
+// // / node index.js//
+
+// function hard (a,s) {
+//    return [a*2,s*2]
+// }
+// let [first,secont] = hard(2,4)
+// console.log(first,secont);
+
+
+
+// // / node index.js//
+// object er kes ber kora
+
+// let ob ={
+//     name: "Azmir",
+//     class : "HSC",
+//     skils : "Web Developer",
+//     selary : 18000
+// }
+
+// let man = Object.keys(ob);
+// console.log(man);
+
+
+
+// // / node index.js//
+// object er value ber kora
+
+// let ob ={
+//     name: "Azmir",
+//     class : "HSC",
+//     skils : "Web Developer",
+//     selary : 18000
+// }
+// let man = Object.values(ob);
+// console.log(man);
+
+
+
+
+// // / node index.js//
+// object er key and value ber kora
+
+// let ob ={
+//     name: "Azmir",
+//     class : "HSC",
+//     skils : "Web Developer",
+//     selary : 18000
+// }
+// let man = Object.entries(ob);
+// console.log(man);
+
+
+
+// // / node index.js//
+// object er value delet kora
+
+// let ob ={
+//     name: "Azmir",
+//     class : "HSC",
+//     skils : "Web Developer",
+//     selary : 18000
+// }
+// // let man = Object.freeze(ob);
+// // delete ob.selary;
+
+// let  {skils, ...buddi} = ob
+// ob.country = "Bangladesh"
+// console.log(buddi);
+
+
+
+
+// // / node index.js//
+// object er value delet kora
+
+// let ob ={
+//     name: "Azmir",
+//     class : "HSC",
+//     skils : "Web Developer",
+//     selary : 18000
+// }
+// let man = Object.seal(ob);
+// ob.selary = 25000
+// console.log(man);
+
+
+
+// // / node index.js//
+// object er loop
+
+// let ob ={
+//     name: "Azmir",
+//     class : "HSC",
+//     skils : "Web Developer",
+//     selary : 18000
+// }
+
+// for(let right in ob){
+//     let val = ob[right]
+//     console.log(right,val);
+// }
+
+
+
+
+
+
+
+// // / node index.js//
+// object er loop
+let ob ={
+    name: "Azmir",
+    class : "HSC",
+    skils : "Web Developer",
+    selary : 18000
+}
+
+let hlw = Object.keys(ob)
+for(let azmir of hlw){
+    let man = ob[azmir]
+    console.log(azmir,man);
+}
+
